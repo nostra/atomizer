a31bdeaf74257c42e277caf9519073d363208798
Erlend Nossum <erlend@api.no>
2013-11-28 13:51:13 +0100

    Create exception hierarchy

--- atomizer-service/src/main/java/no/api/atomizer/exception/AtomizerPathException.java
+++ atomizer-service/src/main/java/no/api/atomizer/exception/AtomizerPathException.java
@@ -4,7 +4,7 @@ package no.api.atomizer.exception;
  * Something went wrong in the web controller on the path the user has given.
  * This usually indicate an error in usage.
  */
-public class AtomizerPathException extends RuntimeException {
+public class AtomizerPathException extends AtomizerException {
 
     public AtomizerPathException(String message) {
         super(message);
--- atomizer-service/src/main/java/no/api/atomizer/exception/AtomizerUnexpectedException.java
+++ atomizer-service/src/main/java/no/api/atomizer/exception/AtomizerUnexpectedException.java
@@ -3,7 +3,7 @@ package no.api.atomizer.exception;
 /**
  * Something entirely not expected happened.
  */
-public class AtomizerUnexpectedException extends RuntimeException {
+public class AtomizerUnexpectedException extends AtomizerException {
     public AtomizerUnexpectedException(String message) {
         super(message);
     }
