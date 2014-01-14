22fc992144e4eb8c2b994a875249c86d5f5c10cd
Erlend Nossum <erlend@abctech-thailand.com>
2014-01-14 14:43:34 +0700

    Revert "Create exception hierarchy"
    This reverts commit a31bdeaf74257c42e277caf9519073d363208798.

--- atomizer-dw-service/src/main/java/no/api/atomizer/exception/AtomizerPathException.java
+++ atomizer-dw-service/src/main/java/no/api/atomizer/exception/AtomizerPathException.java
@@ -4,7 +4,7 @@ package no.api.atomizer.exception;
  * Something went wrong in the web controller on the path the user has given.
  * This usually indicate an error in usage.
  */
-public class AtomizerPathException extends AtomizerException {
+public class AtomizerPathException extends RuntimeException {
 
     public AtomizerPathException(String message) {
         super(message);
--- atomizer-dw-service/src/main/java/no/api/atomizer/exception/AtomizerUnexpectedException.java
+++ atomizer-dw-service/src/main/java/no/api/atomizer/exception/AtomizerUnexpectedException.java
@@ -3,7 +3,7 @@ package no.api.atomizer.exception;
 /**
  * Something entirely not expected happened.
  */
-public class AtomizerUnexpectedException extends AtomizerException {
+public class AtomizerUnexpectedException extends RuntimeException {
     public AtomizerUnexpectedException(String message) {
         super(message);
     }
