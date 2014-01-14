7f71d1895ea663df43bb2556d17e3348f7e5f416
Erlend Nossum <erlend@abctech-thailand.com>
2014-01-14 14:43:25 +0700

    Revert "DEVOPS-223 Improving ping"
    This reverts commit caf9fe6ad594799e8452a4d8df664690c6854e0d.

--- atomizer-dw-mongodao/src/main/java/no/api/atomizer/mongodb/health/MongodbHealth.java
+++ atomizer-dw-mongodao/src/main/java/no/api/atomizer/mongodb/health/MongodbHealth.java
@@ -5,14 +5,11 @@ import com.mongodb.DB;
 import com.mongodb.DBObject;
 import com.mongodb.MongoException;
 import com.yammer.metrics.core.HealthCheck;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  */
 public class MongodbHealth extends HealthCheck {
-    private static final Logger log = LoggerFactory.getLogger(MongodbHealth.class);
 
     private final DB mongodb;
 
@@ -30,15 +27,13 @@ public class MongodbHealth extends HealthCheck {
 
     @Override
     protected Result check(){
-        log.info("Checking mongodb");
         DBObject ping = new BasicDBObject("ping", "1");
         try {
             mongodb.command(ping);
         } catch (MongoException e) {
-            log.info("Failed! Returning unhealthy", e);
             return Result.unhealthy(e);
         }
-        log.info("Success! Returning health");
+
         return Result.healthy();
     }
 }
