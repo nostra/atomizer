8098cf400399d6cae8dafc0a8756ac2216a40d49
Erlend Nossum <erlend@abctech-thailand.com>
2014-01-14 14:43:32 +0700

    Revert "DEVOPS-223 Unsuccessfully trying to add status ping"
    This reverts commit 4c582add2e3597991948cc720e88396be54ce7dd.

--- atomizer-dw-service/src/main/java/no/api/atomizer/amedia/AtomizerPingResource.java
+++ atomizer-dw-service/src/main/java/no/api/atomizer/amedia/AtomizerPingResource.java
@@ -3,7 +3,6 @@ package no.api.atomizer.amedia;
 import com.yammer.dropwizard.jersey.caching.CacheControl;
 import no.api.atomizer.resources.IndexPageResource;
 import no.api.pantheon.io.PantheonFileReader;
-import no.api.pantheon.maven.VersionFileExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +10,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import static javax.ws.rs.core.Response.Status;
 
 /**
  * A different kind of resource than the v3 ping
@@ -29,17 +25,14 @@ public class AtomizerPingResource {
 
     private final int adminPort;
 
-    private final String version;
-
 
     public AtomizerPingResource(int adminPort) {
         this.adminPort = adminPort;
-        this.version = VersionFileExtractor.extractor().whatIsGitVersion();
     }
 
     @GET
     @CacheControl(mustRevalidate = true)
-    public Response doPing() {
+    public String doPing() {
         String result;
         try {
             URL local = new URL("http://localhost:"+adminPort+"/healthcheck");
@@ -47,42 +40,16 @@ public class AtomizerPingResource {
             conn.setReadTimeout(5000);
             int responseCode = conn.getResponseCode();
             if ( responseCode == HttpURLConnection.HTTP_OK ) {
-                return Response.ok("ok "+version, MediaType.TEXT_PLAIN_TYPE).status(new OkStatus("ok "+version)).build();
+                return "OK";
             }
-            result = PantheonFileReader.createInstance().inputStreamToForcedUTF8String(conn.getErrorStream());
+            result = PantheonFileReader.createInstance().inputStreamToForcedUTF8String(local.openStream());
 
         } catch ( IOException e ) {
             log.error("Got exception.", e);
             result = "Got some kind of trouble "+e;
         }
 
-        return Response.ok(result, MediaType.TEXT_PLAIN_TYPE).status(500).build();
-    }
-
-    /**
-     * Not a big success. Seems like this status gets ignored after all.
-     */
-    private class OkStatus implements Response.StatusType {
-
-        private String phrase;
-
-        public OkStatus(String phrase) {
-            this.phrase = phrase;
-        }
-
-        @Override
-        public int getStatusCode() {
-            return HttpURLConnection.HTTP_OK ;
-        }
-
-        @Override
-        public Status.Family getFamily() {
-            return Status.Family.SUCCESSFUL;
-        }
-
-        @Override
-        public String getReasonPhrase() {
-            return phrase;
-        }
+        // TODO Change later to Atomizer exception
+        throw new RuntimeException(""+result);
     }
 }
